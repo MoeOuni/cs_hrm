ac9497207333242012dd9fa36bbb12f86874b769
marouamehrez <maroua.mehrez@yahoo.com>
2023-12-03 22:54:37 +0100

    nouvelle interface demande de congé

--- hrm/DemandeDeConge.Designer.cs
+++ hrm/DemandeDeConge.Designer.cs
@@ -28,12 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DemandeDeConge";
+            textBox4 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            comboBox3 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(158, 153);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(137, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(158, 95);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(137, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(158, 66);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(137, 23);
+            comboBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Date de debut";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(57, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Date de fin";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(57, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Type";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(57, 156);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Raison";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(158, 124);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(137, 23);
+            comboBox3.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(263, 284);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 12;
+            button1.Text = "Modifier";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(344, 284);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 13;
+            button2.Text = "Enregistrer";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(425, 284);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 14;
+            button3.Text = "Envoyer";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // DemandeDeConge
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox3);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox4);
+            Name = "DemandeDeConge";
+            Text = "DemandeDeConge";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private TextBox textBox4;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ComboBox comboBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
